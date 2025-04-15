070f83811ee1d9610a23b1abaa5ab3341c8d6859
intheroomrecordings <info@intheroomrecordings.com>
2025-04-15 15:51:28 +0100

    Add files via upload

---
+++ docs/script.js
@@ -0,0 +1,23 @@
+const players = document.querySelectorAll('.loop-player');
+
+players.forEach(player => {
+  const loopNum = player.getAttribute('data-loop');
+  const playBtn = player.querySelector('.play-btn');
+  const stopBtn = player.querySelector('.stop-btn');
+  const volumeSlider = player.querySelector('.volume-slider');
+  const audio = new Audio(`audio/loop${loopNum}.mp3`);
+  audio.loop = true;
+
+  playBtn.addEventListener('click', () => {
+    audio.play();
+  });
+
+  stopBtn.addEventListener('click', () => {
+    audio.pause();
+    audio.currentTime = 0;
+  });
+
+  volumeSlider.addEventListener('input', () => {
+    audio.volume = volumeSlider.value;
+  });
+});
